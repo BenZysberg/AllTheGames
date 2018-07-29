f48c4fc5f2b1cbd87083aee39eb59783c53aa492
Kyberklemming <bzysberg@gmail.com>
2018-07-28 20:19:49 -0700

    End video

--- public/js/TitleScene.js
+++ public/js/TitleScene.js
@@ -4,10 +4,17 @@ class TitleScene extends Phaser.Scene {
 			key: 'TitleScene'
 		});
 	}
-	preload() {}
+	preload() {
+		this.load.image('backgroundTitle', 'assets/title.jpg');
+		this.load.image('teresaSolo', 'assets/TeresaSolo.png');
+	}
 	create() {
-		this.startKey = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.X);
-		this.bStartKeyDown = false;
+		this.bgTitle = this.add.sprite(0, 0, 'backgroundTitle');
+		this.bgTitle.setOrigin(0, 0);	
+		this.teresa = this.add.sprite(370, 370, 'teresaSolo');
+		this.startText = this.add.text(500, 300, 'PRESS T TO START', { fontFamily: "Nintendo NES Font", fontSize: 32, color: "#000000" });						
+		this.startKey = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.T);
+		this.bStartKeyDown = false;	
 	}
 	update(time, delta) {
 		if(!this.bStartKeyDown)
